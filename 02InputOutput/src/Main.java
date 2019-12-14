893b6b12163756c777b0fce0421fc24e414a6d63
unknown <debas31@gmail.com>
2019-12-14 12:53:19 +0300

    java fundamentals

---
+++ 02InputOutput/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = "";
+        System.out.print("Enter your name: ");
+        name = sc.next();
+        System.out.println("Welcome " + name + " to java programming!");
+
+    }
+}
